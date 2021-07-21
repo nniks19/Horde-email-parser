7c59058b3c872bae33c81b7982f962dc5beccb4b
nniks19 <77620949+nniks19@users.noreply.github.com>
2021-07-21 15:10:31 +0200

    v1.0
    Put extracted email as input and you will get new file emails.txt as output with only emails that you send some messages to

---
+++ Horde Email Finder/Horde Email Finder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Horde_Email_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"INBOX.Sent.MBOX");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("Emails.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.StartsWith("To: <"))
+                {
+                    string sEmail = line.Replace("To: <", "");
+                    sEmail = sEmail.Replace(">", "");
+                    sEmail = sEmail.Replace("mailto:", "");
+                    sw.WriteLine(sEmail);
+                    System.Console.WriteLine(sEmail);
+                    counter++;
+                }
+            }
+            sw.Close();
+            file.Close();
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+    }
+}
